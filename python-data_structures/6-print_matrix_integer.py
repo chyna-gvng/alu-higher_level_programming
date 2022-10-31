a00af33b0ad70aa5d4c71d2d269b28a5ccd40d57
Walter Angoye <w.angoye@alustudent.com>
2022-10-31 04:34:43 -0700

    Mandatory tasks completed

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# function that prints a matrix of integers
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            print("{:d}".format(i[j]), end="")
+            if j < len(i) - 1:
+                print(" ", end="")
+        print("")
---
+++ python-data_structures/README.md
@@ -0,0 +1,3 @@
+### Python Programming
+Readme file for *python-data_structures* directory.  
+Highlight: Data Structures
