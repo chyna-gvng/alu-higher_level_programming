5d4c7fff460792226126be6ac83e032ecf033cd6
Walter Angoye <w.angoye@alustudent.com>
2022-10-31 04:45:43 -0700

    Mandatory tasks completed

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 # function that retrieves an element from a list like in C
 
 def element_at(my_list, idx):
-
     if idx < 0 or idx > (len(my_list) - 1):
         return None
     return (my_list[idx])
