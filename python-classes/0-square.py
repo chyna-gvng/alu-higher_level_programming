494a40c7e321f415a11d2e6c5794d7b298de427e
Walter Angoye <w.angoye@alustudent.com>
2022-11-07 01:05:00 -0800

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 # empty class Square that defines a square
-""
-empty class 'Square' that defines a square
-""
+""define a class 'Square'""
 
 
 class Square:
-    ""
-    empty square
-    ""
+    ""represent a square""
     pass
