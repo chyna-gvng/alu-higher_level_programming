fd5cc4c642c97d0ff7c115ea29548710281899e4
Walter Angoye <w.angoye@alustudent.com>
2022-11-07 02:05:09 -0800

    python-classes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 # class 'Square' that defines a square by:
 # (based on 5-square.py)
+"""
+    define a class 'Square'
+"""
 
 
 class Square:
+    """
+        square with private instance attribute: 'size'
+    """
 
     def __init__(self, size=0, position=(0, 0)):
+        """
+            Args:
+                size (int): size of the new square
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """
+            gets current size of the square
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """
+            validates size is an integer that is greater than zero
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,10 +39,18 @@ class Square:
 
     @property
     def position(self):
+        """
+            gets the current position of the square
+        """
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """
+            set position
+            Args:
+                value: position of the square
+        """
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -35,9 +59,15 @@ class Square:
         self.__position = value
 
     def area(self):
+        """
+            Return: area of the square
+        """
         return (self.__size * self.__size)
 
     def my_print(self):
+        """
+            prints square with character #
+        """
         if self.__size == 0:
             print("")
             return
