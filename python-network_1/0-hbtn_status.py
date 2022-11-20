19bb0767a2cbdd126db42787d31fe58414a49fca
Walter Angoye <w.angoye@alustudent.com>
2022-11-20 22:40:10 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -12,6 +12,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
+"""
+	fetch ://alu-intranet.hbtn.io/status'
+"""
 request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
