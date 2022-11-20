bfacf6c02737352f199162e863cf11fc2c5a13f3
Walter Angoye <w.angoye@alustudent.com>
2022-11-20 22:56:50 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-# python script that fetches 'https://alu-intranet.hbtn.io/status'
-# 	- You must use the package 'urllib'
-# 	- You are not allowed to import any packages other than urllib
-# 	- The body of the response must be displayed like the,
-# 	  following example (tabulation before -)
-# 	- You must use a with statement
-"""
-	- import 'urllib' package
-	- fetch ://alu-intranet.hbtn.io/status'
-"""
-
-
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
+
+
 if __name__ == "__main__":
-	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-		html = response.read()
-		print("Body response:")
-		print("\t- type: {}".format(type(html)))
-		print("\t- content: {}".format(html))
-		print("\t- utf8 content: {}".format(html.decode(encoding="utf-8")))
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
