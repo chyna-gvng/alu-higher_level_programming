c1ae2a900c3036cb72dea71815e167bcd18b29f0
Walter Angoye <w.angoye@alustudent.com>
2022-11-20 22:50:18 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,7 +13,7 @@
 
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
