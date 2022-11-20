9ae28217fe01d897b3087e2d7cfc323b0c635bcb
Walter Angoye <w.angoye@alustudent.com>
2022-11-20 23:03:00 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # python script that fetches 'https://alu-intranet.hbtn.io/status'
+#	- You must use the package urllib
+#	- You are not allowed to import any packages other than urllib
+#	- The body of the response must be displayed like,
+#	..the following example (tabulation before -)
+#	- You must use a with statement
 """
     fetch 'https://intranet.hbtn.io/status'
 """
