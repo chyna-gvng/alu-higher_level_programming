afb869a0c19926097795d5e5dee86887895ff725
Walter Angoye <w.angoye@alustudent.com>
2022-11-28 14:23:45 -0800

    python-object_relational_mapping

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # python file that contains the class definition of a 'State',
 # and an instance 'Base = declarative_base()'
+"""
+   import 'sys' & 'MySQLdb'
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
